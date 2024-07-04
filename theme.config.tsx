09081d594f7a8f0b658fe5299c2a0494628edc66
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-07-04 14:37:18 +0500

    init

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Software Engineer's Swiss Knife</span>,
+  project: {
+    link: 'https://github.com/shakhzodme/sesk',
+  },
+  docsRepositoryBase: "https://github.com/shakhzodme/sesk",
+  footer: {
+    text: "Software Engineer's Swiss Knife: A set of techniques for resolving known or unknown behaviors",
+  },
+  useNextSeoProps: () => ({
+    titleTemplate: "%s - Software Engineer's Swiss Knife"
+  })
+}
+
+export default config
